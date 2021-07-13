095c6d1750f55545704c2f886e74049592a68c7b
Joshua Oladeji <dejijolaoluwa@gmail.com>
2021-07-13 22:04:54 +0100

    changed to ES6+ syntax

--- App.js
+++ App.js
@@ -2,7 +2,9 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+
+
+const App = () => {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -19,3 +21,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+
+
+export default App;
