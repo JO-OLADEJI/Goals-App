d7bf45fff94fb68706a8e01d4e87e30351afd87c
Joshua Oladeji <dejijolaoluwa@gmail.com>
2021-07-15 23:34:21 +0100

    added list of goals

--- App.js
+++ App.js
@@ -1,25 +1,99 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput, ScrollView } from 'react-native';
 
 
 
 const App = () => {
+  const [goal, setGoal] = useState('');
+  const [allGoals, setAllGoals] = useState([]);
+
+  // event handlers
+  const handleGoalSubmit = () => {
+    if (goal !== '') {
+      setAllGoals(currentGoals => [...currentGoals, goal]);
+      setGoal('');
+    }
+  }
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.screen}>
+
+      <Text style={styles.title}>Goals App</Text>
+      <View style={styles.inputBox}>
+        <TextInput 
+          style={styles.textInput}
+          placeholder="Enter a goal..."
+          value={goal}
+          onChangeText={setGoal}
+          keyboardType="default"
+        />
+        <Button 
+          title="Add"
+          onPress={handleGoalSubmit}
+          accessibilityLabel="add new goal"
+        />
+      </View>
+      {(allGoals.length >= 1) && <Text style={styles.subTitle}>Goals</Text>}
+      <ScrollView>
+        {allGoals.map((goal, index) => 
+          <View key={index} style={styles.listItem}>
+            <Text>{goal}</Text>
+          </View>
+        )}
+      </ScrollView>
+
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: {
+    padding: 50,
   },
+
+  textInput: {
+    flex: 1,
+    borderWidth: 1,
+    borderColor: '#000',
+    paddingTop: 7,
+    paddingBottom: 7,
+    paddingLeft: 12,
+    paddingRight: 12,
+    borderRadius: 6,
+    marginRight: 10,
+  },
+
+  title: {
+    fontWeight: 'bold',
+    fontSize: 30,
+    textAlign: 'center',
+    marginBottom: 10,
+  }, 
+
+  subTitle: {
+    fontWeight: 'bold',
+    fontSize: 15,
+    textAlign: 'center',
+    marginTop: 15,
+    color: '#24a0ed',
+  },
+
+  inputBox: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+
+  listItem: {
+    borderWidth: 1,
+    borderRadius: 5,
+    borderColor: '#24a0ed',
+    marginVertical: 5,
+    padding: 5,
+  }
 });
 
 
